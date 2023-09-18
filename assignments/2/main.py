b7b38be7ca7aa3b13f480101fedd79e96cd60ba5
Brayden O'Neal <braydenioneal@gmail.com>
2023-09-18 11:47:24 -0500

    Refactor some lines

--- assignments/2/main.py
+++ assignments/2/main.py
@@ -3,19 +3,11 @@ import csv
 import matplotlib.pyplot as plt
 
 with open('assign2.csv') as data_file:
-    reader = csv.reader(data_file, delimiter=',')
-    next(data_file)
-    x_values = []
-    y_values = []
-    for line in reader:
-        x_values.append(float(line[0]))
-        y_values.append(float(line[1]))
+    data = list(csv.reader(data_file))[1:]
+    x_values = torch.tensor([float(line[0]) for line in data])
+    y_values = torch.tensor([float(line[1]) for line in data])
 
-x_values = torch.tensor(x_values)
-y_values = torch.tensor(y_values)
-
-design_matrix = torch.ones(x_values.size(0), 2)
-design_matrix[:,1] = x_values
+design_matrix = torch.tensor([[1, row] for row in x_values])
 
 least_squares = (
     design_matrix.transpose(0, 1)
@@ -27,7 +19,7 @@ least_squares = (
 intercept = least_squares[0].item()
 slope = least_squares[1].item()
 
-plt.title(f"Slope: {slope:.4}   Intercept: {intercept:.6}")
+plt.title(f'Slope: {slope:.4}   Intercept: {intercept:.6}')
 plt.scatter(x_values, y_values)
-plt.plot(x_values, x_values * slope + intercept, color="red")
+plt.plot(x_values, x_values * slope + intercept, color='red')
 plt.show()
--- assignments/3/ols_tensor_2.py
+++ assignments/3/ols_tensor_2.py
@@ -1,12 +1,12 @@
 import csv
 import torch
 
-with open('temp_co2_data.csv') as csvfile:
-    reader = [[float(item) for item in line] for line in list(csv.reader(csvfile, delimiter=',')).copy()[1:]]
-    x_values = torch.tensor([line[2:] for line in reader])
-    y_values = torch.tensor([[line[1]] for line in reader])
+with open('temp_co2_data.csv') as data_file:
+    data = list(csv.reader(data_file))[1:]
+    x_values = torch.tensor([[float(item) for item in line[2:]] for line in data])
+    y_values = torch.tensor([[float(line[1])] for line in data])
 
-design_matrix = torch.cat((torch.ones(len(x_values), 1), x_values), 1)
+design_matrix = torch.tensor([[1, *line] for line in x_values])
 weights_linear_algebra = torch.linalg.lstsq(design_matrix, y_values, driver='gels').solution[:, 0]
 
 x_mean = x_values.mean(0)
@@ -18,8 +18,7 @@ y_standard_deviation = y_values.std()
 x_values = (x_values - x_mean) / x_standard_deviation
 y_values = (y_values - y_mean) / y_standard_deviation
 
-design_matrix = torch.cat((torch.ones(len(x_values), 1), x_values), 1)
-
+design_matrix = torch.tensor([[1, *row] for row in x_values])
 weights = torch.rand(3, 1) - 0.5 * torch.ones(3, 1)
 
 learning_rate = 0.5
@@ -28,22 +27,20 @@ epochs = 30
 for epoch in range(epochs):
     target_estimates = design_matrix.mm(weights)
 
-    loss = (target_estimates - y_values).pow(2).sum() / len(design_matrix)
-    print(f"epoch: {epoch + 1}, current loss: {loss.item()}")
+    loss = (target_estimates - y_values).pow(2).sum() / design_matrix.size(0)
+    print(f'epoch: {epoch + 1}, current loss: {loss.item()}')
 
-    gradient = 2 * ((target_estimates - y_values) * design_matrix).sum(0, True).t() / len(design_matrix)
-
-    weights = weights - learning_rate * gradient
+    gradient = 2 * ((target_estimates - y_values) * design_matrix).sum(0, True).t() / design_matrix.size(0)
+    weights -= learning_rate * gradient
 
 weights = weights.squeeze(1)
-
 weights[1:] = weights[1:] * y_standard_deviation / x_standard_deviation
 weights[0] = weights[0] * y_standard_deviation + y_mean - weights[1:] @ x_mean
 
 print(
-    f"The least-squares regression plane:\n"
-    f"\tfound by the neural net is: y = {weights[0]:.3f} + {weights[1]:.3f} * x1 + {weights[2]:.3f} * x2\n"
-    f"\tusing linear algebra:\t\ty = {weights_linear_algebra[0]:.3f} + {weights_linear_algebra[1]:.3f} * x1 + "
-    f"{weights_linear_algebra[2]:.3f} * x2\n"
-    f"Learning rate: {learning_rate}\n"
+    f'The least-squares regression plane:\n'
+    f'\tfound by the neural net is: y = {weights[0]:.3f} + {weights[1]:.3f} * x1 + {weights[2]:.3f} * x2\n'
+    f'\tusing linear algebra:\t\ty = {weights_linear_algebra[0]:.3f} + {weights_linear_algebra[1]:.3f} * x1 + '
+    f'{weights_linear_algebra[2]:.3f} * x2\n'
+    f'Learning rate: {learning_rate}\n'
 )
