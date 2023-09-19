fb456ce577f25ddcfe5c07708ffab02baef8e036
Brayden O'Neal <braydenioneal@gmail.com>
2023-09-19 10:28:13 -0500

    Refactor some lines

--- assignments/2/main.py
+++ assignments/2/main.py
@@ -7,19 +7,19 @@ with open('assign2.csv') as data_file:
     x_values = torch.tensor([float(line[0]) for line in data])
     y_values = torch.tensor([float(line[1]) for line in data])
 
-design_matrix = torch.tensor([[1, row] for row in x_values])
+design_matrix = torch.tensor([[1, value] for value in x_values])
 
-least_squares = (
+weights = (
     design_matrix.transpose(0, 1)
     .mm(design_matrix).inverse()
     .mm(design_matrix.transpose(0, 1))
     .mm(y_values.unsqueeze(1))
 )
 
-intercept = least_squares[0].item()
-slope = least_squares[1].item()
+intercept = weights[0].item()
+slope = weights[1].item()
 
-plt.title(f'Slope: {slope:.4}   Intercept: {intercept:.6}')
+plt.title(f'Slope: {slope:.4}\tIntercept: {intercept:.6}')
 plt.scatter(x_values, y_values)
 plt.plot(x_values, x_values * slope + intercept, color='red')
 plt.show()
--- assignments/3/ols_tensor_2.py
+++ assignments/3/ols_tensor_2.py
@@ -3,22 +3,22 @@ import torch
 
 with open('temp_co2_data.csv') as data_file:
     data = list(csv.reader(data_file))[1:]
-    x_values = torch.tensor([[float(item) for item in line[2:]] for line in data])
-    y_values = torch.tensor([[float(line[1])] for line in data])
+    x_features = torch.tensor([[float(item) for item in line[2:]] for line in data])
+    y_features = torch.tensor([[float(line[1])] for line in data])
 
-design_matrix = torch.tensor([[1, *line] for line in x_values])
-weights_linear_algebra = torch.linalg.lstsq(design_matrix, y_values, driver='gels').solution[:, 0]
+design_matrix = torch.tensor([[1, *line] for line in x_features])
+weights_linear_algebra = torch.linalg.lstsq(design_matrix, y_features, driver='gels').solution[:, 0]
 
-x_mean = x_values.mean(0)
-y_mean = y_values.mean()
+x_mean = x_features.mean(0)
+y_mean = y_features.mean()
 
-x_standard_deviation = x_values.std(0)
-y_standard_deviation = y_values.std()
+x_standard_deviation = x_features.std(0)
+y_standard_deviation = y_features.std()
 
-x_values = (x_values - x_mean) / x_standard_deviation
-y_values = (y_values - y_mean) / y_standard_deviation
+x_features = (x_features - x_mean) / x_standard_deviation
+y_features = (y_features - y_mean) / y_standard_deviation
 
-design_matrix = torch.tensor([[1, *row] for row in x_values])
+design_matrix = torch.tensor([[1, *line] for line in x_features])
 weights = torch.rand(3, 1) - 0.5 * torch.ones(3, 1)
 
 learning_rate = 0.5
@@ -27,10 +27,10 @@ epochs = 30
 for epoch in range(epochs):
     target_estimates = design_matrix.mm(weights)
 
-    loss = (target_estimates - y_values).pow(2).sum() / design_matrix.size(0)
+    loss = (target_estimates - y_features).pow(2).sum() / design_matrix.size(0)
     print(f'epoch: {epoch + 1}, current loss: {loss.item()}')
 
-    gradient = 2 * ((target_estimates - y_values) * design_matrix).sum(0, True).t() / design_matrix.size(0)
+    gradient = 2 * ((target_estimates - y_features) * design_matrix).sum(0, True).t() / design_matrix.size(0)
     weights -= learning_rate * gradient
 
 weights = weights.squeeze(1)
