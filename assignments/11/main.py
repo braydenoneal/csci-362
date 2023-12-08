6b12ed814fdb896bc6e90e1d600bb3985fd82951
Brayden O'Neal <braydenioneal@gmail.com>
2023-12-08 11:34:00 -0600

    Update assignment 11 with cross-validation

--- assignments/11/main.py
+++ assignments/11/main.py
@@ -7,17 +7,10 @@ import math
 train_amount = 0.9
 learning_rate = 0.01
 momentum = 0.53
-epochs = 256
+epochs = 96
 batch_size = 64
 centered = True
 normalized = True
-
-# 97%
-# train_amount = 0.9
-# learning_rate = 0.1
-# momentum = 0.53
-# epochs = 256
-# batch_size = 32
 
 digits = io.imread('digits.png')
 xss = torch.Tensor(5000, 20, 20)
@@ -40,9 +33,13 @@ xss_test = xss[random_split][train_split_amount:]
 if centered:
     xss_train, xss_train_means = dulib.center(xss_train)
     xss_test, _ = dulib.center(xss_test, xss_train_means)
+
+    xss, xss_means = dulib.center(xss)
 if normalized:
     xss_train, xss_train_stds = dulib.normalize(xss_train)
     xss_test, _ = dulib.normalize(xss_test, xss_train_stds)
+
+    xss, xss_stds = dulib.normalize(xss)
 
 yss_train = yss[random_split][:train_split_amount]
 yss_test = yss[random_split][train_split_amount:]
@@ -51,21 +48,6 @@ yss_test = yss[random_split][train_split_amount:]
 class ConvolutionalModel(nn.Module):
     def __init__(self):
         super(ConvolutionalModel, self).__init__()
-        """
-        L1
-        5000 in 20, 20
-        
-        16 out 20, 20
-        
-        16 out 10, 10
-        
-        L2
-        5000 * 16 in 10, 10
-        
-        16 out 10, 10
-        
-        16 out 5, 5
-        """
         self.meta_layer1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
@@ -103,18 +85,22 @@ def pct_correct(xss_test_, yss_test_):
     return 100 * count / len(xss_test_)
 
 
-model = dulib.train(
+model, valids = dulib.cv_train(
     model,
     crit=criterion,
-    train_data=(xss_train, yss_train),
-    valid_data=(xss_test, yss_test),
+    train_data=(xss, yss),
     learn_params={'lr': learning_rate, 'mo': momentum},
     epochs=epochs,
     bs=batch_size,
-    valid_metric=pct_correct,
-    graph=1,
-    print_lines=(-1,)
+    verb=10,
+    k=8,
 )
+
+print(model)
+print(valids)
+
+print('\nAll Data Confusion Matrix\n')
+pct_all = dulib.class_accuracy(model, (xss, yss), show_cm=True)
 
 print('\nTraining Data Confusion Matrix\n')
 pct_training = dulib.class_accuracy(model, (xss_train, yss_train), show_cm=True)
@@ -124,6 +110,7 @@ pct_testing = dulib.class_accuracy(model, (xss_test, yss_test), show_cm=True)
 
 print(
     f'\n'
+    f'Percentage correct on all data: {100 * pct_all:.2f}\n'
     f'Percentage correct on training data: {100 * pct_training:.2f}\n'
     f'Percentage correct on testing data: {100 * pct_testing:.2f}\n'
     f'\n'
