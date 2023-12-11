f543ec6ce1cc9ad45a692d0672e497a88dbd99a9
Brayden O'Neal <braydenioneal@gmail.com>
2023-12-11 09:34:41 -0600

    Update assignment 11

--- assignments/11/main.py
+++ assignments/11/main.py
@@ -4,21 +4,40 @@ from skimage import io
 import du.lib as dulib
 import math
 
-train_amount = 0.9
-learning_rate = 0.01
+train_amount = 0.8
+learning_rate = 0.1
 momentum = 0.53
 epochs = 96
-batch_size = 64
+batch_size = 32
 centered = True
 normalized = True
 
+# 97.10%
+# train_amount = 0.8
+# learning_rate = 0.01
+# momentum = 0.9
+# epochs = 128
+# batch_size = 32
+# centered = True
+# normalized = True
+
+# 98.10%
+# train_amount = 0.8
+# learning_rate = 0.1
+# momentum = 0.53
+# epochs = 96
+# batch_size = 32
+# centered = True
+# normalized = True
+
 digits = io.imread('digits.png')
 xss = torch.Tensor(5000, 20, 20)
-idx = 0
+xss_index = 0
+
 for i in range(0, 1000, 20):
     for j in range(0, 2000, 20):
-        xss[idx] = torch.Tensor((digits[i:i + 20, j:j + 20]))
-        idx = idx + 1
+        xss[xss_index] = torch.Tensor((digits[i:i + 20, j:j + 20]))
+        xss_index = xss_index + 1
 
 yss = torch.LongTensor(len(xss))
 for i in range(len(yss)):
@@ -34,12 +53,9 @@ if centered:
     xss_train, xss_train_means = dulib.center(xss_train)
     xss_test, _ = dulib.center(xss_test, xss_train_means)
 
-    xss, xss_means = dulib.center(xss)
 if normalized:
     xss_train, xss_train_stds = dulib.normalize(xss_train)
     xss_test, _ = dulib.normalize(xss_test, xss_train_stds)
-
-    xss, xss_stds = dulib.normalize(xss)
 
 yss_train = yss[random_split][:train_split_amount]
 yss_test = yss[random_split][train_split_amount:]
@@ -74,33 +90,17 @@ class ConvolutionalModel(nn.Module):
 model = ConvolutionalModel()
 criterion = nn.NLLLoss()
 
-
-def pct_correct(xss_test_, yss_test_):
-    count = 0
-
-    for x, y in zip(xss_test_, yss_test_):
-        if torch.argmax(x).item() == y.item():
-            count += 1
-
-    return 100 * count / len(xss_test_)
-
-
 model, valids = dulib.cv_train(
     model,
     crit=criterion,
-    train_data=(xss, yss),
+    train_data=(xss_train, yss_train),
     learn_params={'lr': learning_rate, 'mo': momentum},
     epochs=epochs,
     bs=batch_size,
     verb=10,
-    k=8,
+    k=10,
+    bail_after=30,
 )
-
-print(model)
-print(valids)
-
-print('\nAll Data Confusion Matrix\n')
-pct_all = dulib.class_accuracy(model, (xss, yss), show_cm=True)
 
 print('\nTraining Data Confusion Matrix\n')
 pct_training = dulib.class_accuracy(model, (xss_train, yss_train), show_cm=True)
@@ -110,12 +110,15 @@ pct_testing = dulib.class_accuracy(model, (xss_test, yss_test), show_cm=True)
 
 print(
     f'\n'
-    f'Percentage correct on all data: {100 * pct_all:.2f}\n'
     f'Percentage correct on training data: {100 * pct_training:.2f}\n'
     f'Percentage correct on testing data: {100 * pct_testing:.2f}\n'
     f'\n'
+    f'Valids: {valids}\n'
+    f'Train Amount: {train_amount * 100}%\n'
     f'Learning Rate: {learning_rate}\n'
     f'Momentum: {momentum}\n'
     f'Epochs: {epochs}\n'
-    f'Batch Size: {batch_size}'
+    f'Batch Size: {batch_size}\n'
+    f'Centered: {centered}\n'
+    f'Normalized: {normalized}'
 )
